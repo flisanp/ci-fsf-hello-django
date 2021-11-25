af8392e980ac0d740e95854ecd34aef9bda80996
flisanp <ida@ofstories.se>
2021-11-25 12:38:19 +0000

    change allowed_hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-+vx+!9e8#*bw6n&=oh54i!ar=j4@f5@x5$oa(#wh2kj+3&v(0v')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+vx+!9e8#*bw6n&=oh54i!ar=j4@f5@x5$oa(#wh2kj+3&v(0v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['ida1985-django-todo-app.herokuapp.com']
+
 
 
 # Application definition
